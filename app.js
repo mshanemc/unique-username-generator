b107d1cb12d8bacab688f517475ff55912c817c8
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-28 10:14:53 -0600

    simplify

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var memjs = require('memjs')
@@ -10,20 +9,14 @@ var mc = memjs.Client.create()
 
 var app = express();
 var router = express.Router();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 // catch 404 and forward to error handler
-router.post('/unique', (req, res) => {
+app.post('/unique', (req, res) => {
   console.log('heard post');
   const prefix = req.body.prefix;
   const domain = req.body.domain;

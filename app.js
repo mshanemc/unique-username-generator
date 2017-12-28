74474fd74efe9a2316e63f0517348e9a9750ccf3
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-28 10:08:17 -0600

    switch to router

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mc = memjs.Client.create()
 
 
 var app = express();
+var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +21,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.post('/unique', (req, res) => {
+router.post('/unique', (req, res) => {
   const prefix = req.body.prefix;
   const domain = req.body.domain;
   mc.incr(`${prefix}/${domain}`, 1, function (err, value) {

a6a69a9e693e2fd571eda265ae1ffe9fa8e4edfb
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-28 10:06:42 -0600

    unused stuff removal

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 var memjs = require('memjs')
 var mc = memjs.Client.create()
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -24,28 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
 app.post('/unique', (req, res) => {
   const prefix = req.body.prefix;
   const domain = req.body.domain;
